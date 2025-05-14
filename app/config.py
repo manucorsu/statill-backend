2947ba79d8f23582623d7659292234f4c029906b
manucorsu <142500898+manucorsu@users.noreply.github.com>
2025-05-14 08:59:23 -0300

    begin setup

--- app/config.py
+++ app/config.py
@@ -1,1 +1,13 @@
-# Settings using Pydantic/BaseSettings
+from lib2to3.pytree import Base
+from pydantic import BaseSettings
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    database_url: str = getenv("DATABASE_URL")
+
+
+settings = Settings()
--- app/database/session.py
+++ app/database/session.py
@@ -1,1 +1,7 @@
-# SQLAlchemy engine/session creation
+from zipapp import create_archive
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.config import settings
+
+engine = create_engine(settings.database_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
