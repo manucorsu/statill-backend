b2f78e7701156f474df2604772f566d31461916d
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2025-05-15 10:41:35 -0300

    Tablas user y store creadas

---
+++ app/models/user.py
@@ -0,0 +1,29 @@
+import string
+from app.database.base import Base
+from sqlalchemy import Column, Integer, String, BigInteger, Date, Enum, Boolean
+import enum
+
+class GenderEnum(enum.Enum):
+    MALE = "M"
+    FEMALE = "F"
+    OTHER = "X"
+
+class StoreRoleEnum(enum.Enum):
+    CASHIER = "cashier"
+    OWNER = "owner"
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(BigInteger, primary_key=True)
+    first_names = Column(String(40), nullable=False)
+    last_name = Column(String(40), nullable=False)
+    email = Column(String(255), nullable=False)
+    password = Column(String, nullable=False)
+    birthdate = Column(Date, nullable=False)
+    gender = Column(Enum(GenderEnum, name = "gender_enum"), nullable=False)
+    res_area = Column(String(50), nullable=False)
+    is_admin = Column(Boolean, nullable=False)
+    store_id = Column(BigInteger)
+    store_role = Column(Enum(StoreRoleEnum, name = "store_role_enum"), nullable=False)
+
+
