d3f96fd489fb68001eb3bb4d864dbb60f764ca5b
manucorsu <142500898+manucorsu@users.noreply.github.com>
2025-05-15 10:42:15 -0300

    create declarative base

--- app/config.py
+++ app/config.py
@@ -1,4 +1,3 @@
-from lib2to3.pytree import Base
 from pydantic import BaseSettings
 from dotenv import load_dotenv
 from os import getenv
--- app/database/base.py
+++ app/database/base.py
@@ -1,1 +1,5 @@
-# SQLAlchemy declarative base
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
