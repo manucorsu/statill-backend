65dc70c9d60861faef0c70bbc2b92be8688171a7
manucorsu <142500898+manucorsu@users.noreply.github.com>
2025-05-22 12:07:44 -0300

    fix for cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from app.api.v1 import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
+from .config import settings
 
 app = FastAPI(title="Statill API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.frontend_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api/v1")
