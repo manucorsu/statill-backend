309b3f85d31f67a1d6060bf6e7737b0ea5206cb3
manucorsu <142500898+manucorsu@users.noreply.github.com>
2025-06-26 10:45:51 -0300

    merge 2

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -1,31 +1,31 @@
-from fastapi import APIRouter, Depends
+# from fastapi import APIRouter, Depends
 
-from ...schemas.general import APIResponse
-from ...schemas.user import UserCreate, GetAllUsersResponse, GetUserResponse
+# from ...schemas.general import APIResponse
+# from ...schemas.user import UserCreate, GetAllUsersResponse, GetUserResponse
 
-from ...dependencies.db import get_db
+# from ...dependencies.db import get_db
 
-from sqlalchemy.orm import Session
+# from sqlalchemy.orm import Session
 
-from crud import user as crud
+# from crud import user as crud
 
-router = APIRouter()
+# router = APIRouter()
 
 
-@router.get("/", response_model=GetAllUsersResponse)
-def get_all_users(session: Session = Depends(get_db)):
-    """
-    Retrieves all users from the database.
+# @router.get("/", response_model=GetAllUsersResponse)
+# def get_all_users(session: Session = Depends(get_db)):
+#     """
+#     Retrieves all users from the database.
 
-    (Will require auth in the future)
-    (Will require admin role in the future)
-    Args:
-        session (Session): The SQLAlchemy session to use for the query.
-    Returns:
-        GetAllUsersResponse: A response containing a list of all sales.
-    """
-    result = crud.get_all(session)
+#     (Will require auth in the future)
+#     (Will require admin role in the future)
+#     Args:
+#         session (Session): The SQLAlchemy session to use for the query.
+#     Returns:
+#         GetAllUsersResponse: A response containing a list of all sales.
+#     """
+#     result = crud.get_all(session)
 
-    return GetAllUsersResponse(
-        successful=True, data=result, message="Successfully retrieved all Users."
-    )
+#     return GetAllUsersResponse(
+#         successful=True, data=result, message="Successfully retrieved all Users."
+#     )
