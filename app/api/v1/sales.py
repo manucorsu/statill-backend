67597473557e04d181d88710cdd7f2458af14834
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2025-06-26 10:05:14 -0300

    sale create modifies stock

--- app/api/v1/sales.py
+++ app/api/v1/sales.py
@@ -59,69 +59,18 @@ def get_sale_by_id(id: int, db: Session = Depends(get_db)):
 
 @router.post("/", response_model=APIResponse, status_code=201)
 def create_sale(sale: SaleCreate, db: Session = Depends(get_db)):
-    
+    """
+    Creates a sale.
+
+    (Will require auth in the future)
+
+    Args:
+        sale (SaleCreate): The sale data.
+        db (Session): The SQLAlchemy session to use for the query.
+    """
     sale_id = crud.create_sale_with_products(sale, db)
     return APIResponse(
         successful=True,
         data={"id": sale_id},
         message=f"Successfully created the Sale, which received id {sale_id}.",
     )
-
-
-
-
-@router.put("/{id}", response_model=APIResponse)
-def update_sale(id: int, sale: SaleCreate, db: Session = Depends(get_db)):
-    """
-    Updates a sale by its ID.
-
-    (Will require auth in the future)
-
-    Args:
-       id (int): The ID of the sale to update.
-       sale (SaleCreate): The updated sale data.
-       db (Session): The SQLAlchemy session to use for the update.
-    
-    Returns:
-        APIResponse: A response indicating the success of the update operation.
-    
-    Raises:
-        HTTPException(400): If the provided ID is invalid (less than or equal to 0).
-        HTTPException(404): If the sale with the specified ID does not exist.
-    """
-    if id <= 0:
-        raise HTTPException(status_code=400, detail="Invalid id.")
-
-    crud.update_by_id(id, sale, db)
-    return APIResponse(
-        successful=True, data=None, message="Successfully updated the Sale."
-    )
-
-
-@router.delete("/{id}", response_model=APIResponse)
-def delete_sale_by_id(id: int, db: Session = Depends(get_db)):
-    """
-    Deletes a sale by its ID.
-    (Will require auth in the future)
-
-    Args:
-        id (int): The ID of the sale to delete.
-        db (Session): The SQLAlchemy session to use for the delete.
-
-    Returns:
-        APIResponse: A response indicating the success of the delete operation.
-
-    Raises:
-        HTTPException(400): If the provided ID is invalid (less than or equal to 0).
-        HTTPException(404): If the sale with the specified ID does not exist.
-    """
-    if id <= 0:
-        raise HTTPException(status_code=400, detail="Invalid id.")
-        
-    crud.delete_by_id(id, db)
-    return APIResponse(
-        successful=True,
-        data=None,
-        message=f"Successfully deleted the Sale with id {id}.",
-    )
-
