d8eca4c1ac369ed15fdd377e213f8529739a6b45
manucorsu <142500898+manucorsu@users.noreply.github.com>
2025-07-04 19:24:30 -0300

    /api/v1/docs -> /docs and /api/v1/redoc -> /redoc redirects

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, responses
 from . import status, products, sales, users, stores
 
 router = APIRouter()
+
+
+@router.get("/docs", response_class=responses.RedirectResponse, status_code=308)
+def get_docs_redirect():
+    return "/docs"
+
+
+@router.get("/redoc", response_class=responses.RedirectResponse, status_code=308)
+def get_redoc_redirect():
+    return "/redoc"
+
 
 router.include_router(status.router, prefix="/status", tags=["status"])
 router.include_router(products.router, prefix="/products", tags=["products"])
