365a14345c1958c90b543125413522b872be8185
manucorsu <142500898+manucorsu@users.noreply.github.com>
2025-08-07 10:42:50 -0300

    empezar a hacer tests como la gente

---
+++ tests/api/test_status.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+def test_get_status():
+    response = client.get("/api/v1/status")
+    assert response.status_code == 200
+    assert response.json() == {"successful":True,"data":{"status":"ok"},"message":"Successfully performed a status check."}
+
---
+++ tests/scripts/run.ps1
@@ -0,0 +1,7 @@
+pytest --cov=app --cov-report=html
+# $scriptLocation = $PSScriptRoot
+# cd ..
+# cd ..
+# $projectRoot = (Get-Location).Path
+# Write-Output "Coverage en $projectRoot\htmlcov\index.html"
+# cd $scriptLocation
