cf2449ee523340f67b7638c35e808bf3cf78cf7f
manucorsu <142500898+manucorsu@users.noreply.github.com>
2025-09-29 14:24:52 -0300

    create_discount al desundo

--- app/api/v1/discounts.py
+++ app/api/v1/discounts.py
@@ -3,7 +3,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from ...crud import discount as crud
-from ...schemas.discount import GetAllDiscountsResponse
+from ...schemas.general import APIResponse
+from ...schemas.discount import GetAllDiscountsResponse, DiscountCreate
 from ...dependencies.db import get_db
 
 name = "discounts"
@@ -26,4 +27,24 @@ def get_all_discounts(session: Session = Depends(get_db)):
     result = crud.get_all(session)
     return GetAllDiscountsResponse(
         successful=True, data=result, message="Successfully retrieved all discounts."
+    )
+
+
+@router.post("/", response_model=APIResponse)
+def create_discount(discount: DiscountCreate, session=Depends(get_db)):
+    """
+    Creates a discount.
+
+    Args:
+        discount (DiscountCreate): The discount data.
+        session (Session): The SQLAlchemy session to use for the query.
+    Returns:
+        (APIResponse) An APIResponse containing the new discount's id.
+    """
+    # TODO URGENTE AGREGAR LOS CHECKS QUE FALTAN!
+    id = crud.create(discount, session)
+    return APIResponse(
+        successful=True,
+        data={"id": id},
+        message=f"Successfully created the Discount, which received id {id}.",
     )
