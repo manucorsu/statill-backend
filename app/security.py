06b879567e81a6dba0d6f941650f63f2f5265865
manucorsu <142500898+manucorsu@users.noreply.github.com>
2025-10-16 17:10:14 -0300

    security.py sin testeare

---
+++ app/security.py
@@ -0,0 +1,76 @@
+from argon2 import PasswordHasher
+from datetime import datetime, timezone, timedelta
+from config import settings
+import jwt
+from typing import Any
+from fastapi import HTTPException
+
+__ph = PasswordHasher()
+
+
+def hash(password: str) -> str:
+    """
+    Hashes a password using `argon2-cffi`.
+
+    Args:
+        password (str): The plain text password to hash.
+    Returns:
+        str: The hashed password.
+    """
+    return __ph.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Verifies a plain text password against a hashed password.
+
+    Args:
+        plain_password (str): The plain text password to verify.
+        hashed_password (str): The hashed password to verify against.
+    Returns:
+        bool: True if the password matches, False otherwise.
+    """
+    try:
+        return __ph.verify(hashed_password, plain_password)
+    except:
+        return False
+
+
+def create_token(subject, expires_delta: int = settings.jwt_expiry) -> str:
+    """
+    Creates a JWT token.
+    Args:
+        subject (Any): (Any but will be converted to `str(subject)`) The subject of the token (usually a user id).
+        expires_delta (int): The expiration time in minutes. Default is set in settings.
+    Returns:
+        str: The encoded JWT token.
+    Raises:
+        ValueError: If expires_delta is not a positive integer.
+    """
+    if expires_delta < 1:
+        raise ValueError("expires_delta must be a positive integer")
+    now = datetime.now(timezone.utc)
+    expire = now + timedelta(minutes=expires_delta)
+    payload = {"sub": str(subject), "iat": now.timestamp(), "exp": expire.timestamp()}
+
+    token = jwt.encode(payload, settings.jwt_secret, algorithm=settings.jwt_algorithm)
+    return token
+
+
+def decode_token(token: str) -> Any:
+    """
+    Decodes a JWT token.
+    Args:
+        token (str): The JWT token to decode.
+    Returns:
+        Any: The decoded token payload.
+    Raises:
+        HTTPException(401): If the token is invalid or expired.
+    """
+    try:
+        payload = jwt.decode(
+            token, settings.jwt_secret, algorithms=[settings.jwt_algorithm]
+        )
+        return payload
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="Unauthorized")
